54b3fde870c10bb989975e6d5de1f05979f7753a
Chaxiraxi <david.goletta@live.com>
2025-07-31 01:43:59 +0200

    Bump plugin version to 1.1.0

--- CardHacker/PluginInfo.cs
+++ CardHacker/PluginInfo.cs
@@ -4,7 +4,7 @@ namespace ROUNDSCheat
     {
         public const string PLUGIN_GUID = "Chaxiraxi.CardHacker";
         public const string PLUGIN_NAME = "CardHacker";
-        public const string PLUGIN_VERSION = "1.0.2";
+        public const string PLUGIN_VERSION = "1.1.0";
         public const string PLUGIN_DESCRIPTION = "A client-side cheat that allows you to choose any card you want to play in ROUNDS";
         public const string PLUGIN_AUTHOR = "Chaxiraxi";
     }
