6a6cb7fa42f3f41ee9aedca1b4f574114783d526
Chaxiraxi <david.goletta@live.com>
2025-07-30 02:36:31 +0200

    Update plugin version to 1.0.1 in PluginInfo

--- ROUNDS-Cheat/PluginInfo.cs
+++ ROUNDS-Cheat/PluginInfo.cs
@@ -4,7 +4,7 @@ namespace ROUNDSCheat
     {
         public const string PLUGIN_GUID = "Chaxiraxi.CardHacker";
         public const string PLUGIN_NAME = "CardHacker";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
         public const string PLUGIN_DESCRIPTION = "A client-side cheat that allows you to select a card to spawn in the card choice GUI, overriding the default random selection.";
         public const string PLUGIN_AUTHOR = "Chaxiraxi";
     }
